1bc3ebee5415808167fe0769df354801956ad556
Abraham Barragan Lira <abraham.barragan02@gmail.com>
2022-12-11 18:31:31 -0700

    Modificacion en program server

---
+++ BlazorContactos/Server/Migrations/20221212013032_Contactos-Medios.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorContactos.Server.Migrations
+{
+    public partial class ContactosMedios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contactos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contactos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Medios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Telefono = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Celular = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ContactoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Medios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Medios_Contactos_ContactoId",
+                        column: x => x.ContactoId,
+                        principalTable: "Contactos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Medios_ContactoId",
+                table: "Medios",
+                column: "ContactoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Medios");
+
+            migrationBuilder.DropTable(
+                name: "Contactos");
+        }
+    }
+}
